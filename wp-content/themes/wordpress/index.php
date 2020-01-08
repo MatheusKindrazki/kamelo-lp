ed9bea548e8cb561aff47befeffab7444eafb912
therealeddy <eduardo.mfonseca@hotmail.com>
2020-01-08 20:29:58 -0300

    objetivos completo

--- wp-content/themes/wordpress/index.php
+++ wp-content/themes/wordpress/index.php
@@ -3,10 +3,8 @@
 <section id="home">
     <?php get_template_part('parts/banner') ?>
     <?php get_template_part('parts/sobre') ?>
-    <?php //get_template_part('parts/objetivos') ?>
-    <?php //get_template_part('parts/cadastro') ?>
-
-    <div class="section">oie</div>
+    <?php get_template_part('parts/objetivos') ?>
+    <?php get_template_part('parts/cadastro') ?>
 <section>
 
 <?php get_footer(); ?>
