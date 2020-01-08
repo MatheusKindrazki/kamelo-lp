37f9ad891dde179ed28ea0c5ce18e0b7c8b2d06c
therealeddy <eduardo.mfonseca@hotmail.com>
2020-01-08 19:50:30 -0300

    banner semi completo

--- wp-content/themes/wordpress/header.php
+++ wp-content/themes/wordpress/header.php
@@ -1,1 +1,8 @@
 <?php get_template_part('parts/head'); ?>
+
+<header>
+    <a href="<?php bloginfo('url') ?>">
+        <img src="<?php echo get_field('logotipo', 'options') ?>" alt="logo">
+    </a>
+    <?php wp_nav_menu(); ?>
+</header>
