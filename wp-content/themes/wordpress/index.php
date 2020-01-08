2d9af94a4340c2e4fb320ebcdefc50f89771d337
therealeddy <eduardo.mfonseca@hotmail.com>
2020-01-08 20:14:34 -0300

    sobre completo

--- wp-content/themes/wordpress/index.php
+++ wp-content/themes/wordpress/index.php
@@ -2,7 +2,7 @@
 
 <section id="home">
     <?php get_template_part('parts/banner') ?>
-    <?php //get_template_part('parts/sobre') ?>
+    <?php get_template_part('parts/sobre') ?>
     <?php //get_template_part('parts/objetivos') ?>
     <?php //get_template_part('parts/cadastro') ?>
 
---
+++ wp-content/themes/wordpress/parts/sobre.php
@@ -0,0 +1,18 @@
+<div class="section">
+    <div id="sobre">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2 class="title-2">Loren ipsum dolor Kamelo Whithout app.</h2>
+                    <p>
+                        Chat app messaging and powerful than any other 
+                        application. you can enjoy free call and free chat
+                        anytime and forever
+                    </p>
+                    <a href="#!" target="_blank" class="btn-theme purple">Cadastre-se</a>
+                </div>
+            </div>
+        </div>
+        <img src="<?php bloginfo('template_directory') ?>/images/iphone-sobre.png" alt="iphone" class="phone">
+    </div>
+</div>
